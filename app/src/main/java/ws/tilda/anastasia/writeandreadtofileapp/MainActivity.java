41c45a2cabe43a56c93bb7785cd6a520260a8630
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-09-23 15:07:53 +0300

    Initial commit

---
+++ app/src/main/java/ws/tilda/anastasia/writeandreadtofileapp/MainActivity.java
@@ -0,0 +1,13 @@
+package ws.tilda.anastasia.writeandreadtofileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
